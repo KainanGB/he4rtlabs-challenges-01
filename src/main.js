6596bbedd59852ef71885dee148b69d6beb335e7
unknown <kainanytbr@gmail.com>
2020-02-23 02:40:17 -0300

    js logic improved

--- src/main.js
+++ src/main.js
@@ -1,48 +1,54 @@
-const horasDia = document.getElementById('horas-dia')
-const diasSemana = document.getElementById('dias-semana')
-const feriasProjeto = document.getElementById('ferias-projeto')
-const valorTotal = document.getElementById('total-value')
 const button = document.getElementById('button')
-const footer = document.querySelector('footer')
+const resultado = document.getElementById('resultado')
+const hide = document.querySelector('.result')
+
+
 
 button.addEventListener('click', () =>{
-  let hoursPerDay = horasDia.value;
-  let daysPerWeek = diasSemana.value;
-  let vacPerProject = feriasProjeto.value;
-  let totalValue = valorTotal.value;
+   const horasDia = document.querySelectorAll('.inputs')[0].value
+    diasSemana = document.querySelectorAll('.inputs')[1].value,
+    ferias = document.querySelectorAll('.inputs')[2].value,
+    valorTotal = document.querySelectorAll('.inputs')[3].value
+   
+   let valorHora = valorTotal / (diasSemana * 4 * horasDia) + ( ferias * diasSemana * horasDia)
+   
+    if(isNaN(valorHora)){
+      let hidden = () => hide.setAttribute('style', 'opacity: 0;');
+      hide.removeAttribute('style')
+      setTimeout(hidden, 5000)
+        return resultado.innerHTML= 'Preencha todos os valores corretamente'.fontcolor('#c295d8')
+   } 
+   else {
+    hide.removeAttribute('style')
+     return resultado.innerHTML= `Valor p/hora: ${valorHora.toFixed(2)}R$`
+  }
+})
 
-  if(isNaN(hoursPerDay)){
-    horasDia.value = ''
-    horasDia.style.fontWeight ='800'
-    horasDia.placeholder = 'Digite um valor de 1 a 24'
-    button.preventDefault();
-  } else if(isNaN(daysPerWeek) ){
-    diasSemana.value = ''
-    diasSemana.style.fontWeight ='800'
-    diasSemana.placeholder = 'Digite um valor de 1 a 7'
-    button.preventDefault();
-  } else if(isNaN(vacPerProject) ){
-    feriasProjeto.value = ''
-    feriasProjeto.style.fontWeight ='800'
-    feriasProjeto.style.fontSize = '13px'
-    feriasProjeto.placeholder = 'Digite um valor númerico'
-    button.preventDefault();
-  } else if(isNaN(totalValue) ){
-    valorTotal.value = ''
-    valorTotal.style.fontWeight ='800'
-    valorTotal.style.fontSize = '13px'
-    valorTotal.placeholder = 'Digite um valor númerico'
-    button.preventDefault();
-  }
-  let valorHora = totalValue / (daysPerWeek * 4 * hoursPerDay) + ( vacPerProject * daysPerWeek * hoursPerDay)
-  if (isNaN(valorHora)){
-    button.preventDefault();
-  }else {
-    let valorHora = totalValue / (daysPerWeek * 4 * hoursPerDay) + ( vacPerProject * daysPerWeek * hoursPerDay)
-    const resultado = document.getElementById('resultado').innerHTML= `O seu valor por hora é de : ${valorHora.toFixed(2)}R$`
-    footer.style.marginTop = '82px';
-  }
+// let valorHora = totalValue / (daysPerWeek * 4 * hoursPerDay) + ( vacPerProject * daysPerWeek * hoursPerDay)
+
+
+
+
+
+
+
+
+
+// button.addEventListener('click', () =>{
+//   const horasDia = document.querySelectorAll('.inputs')[0].value
+//   const diasSemana = document.querySelectorAll('.inputs')[1].value
+//   const ferias = document.querySelectorAll('.inputs')[2].value
+//   const valorTotal = document.querySelectorAll('.inputs')[3].value
   
-});
+//   let valorHora = valorTotal / (diasSemana * 4 * horasDia) + ( ferias * diasSemana * horasDia)
 
-
+  
+//    if(isNaN(valorHora)){
+//    hide.removeAttribute('style')
+//    return resultado.innerHTML= 'Prencha todos os valores corretamente'.fontcolor('#c295d8')
+//   } else {
+//    hide.removeAttribute('style')
+//    resultado.style.fontWeight = 800
+//     return resultado.innerHTML= `Valor p/hora: ${valorHora.toFixed(2)}R$`
+//  }
+// })
